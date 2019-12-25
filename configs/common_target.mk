a1d2e33fa487e03f958322cfb08b17643ea2b51d
zhenquan.qiu <gnsyxiang@163.com>
2019-12-25 14:27:00 +0800

    revert: fix: open output log

--- configs/common_target.mk
+++ configs/common_target.mk
@@ -18,13 +18,13 @@
 #     last modified: 15/11 2019 13:47
 # ===============================================================
 
-#err_no_targets:
-#	@echo "error: use \"targets = your_target\" to specify your target to make!"
-#	exit 1
-#
-#ifeq ($(V),1)
-#slient_targets=err_no_targets
-#endif
-#
-#.SILENT: $(slient_targets)
+err_no_targets:
+	@echo "error: use \"targets = your_target\" to specify your target to make!"
+	exit 1
+
+ifeq ($(V),1)
+slient_targets=err_no_targets
+endif
+
+.SILENT: $(slient_targets)
 
