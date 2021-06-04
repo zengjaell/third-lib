bea34de0155abf0598dbb99504b877cda39a765d
zhenquan.qiu <gnsyxiang@163.com>
2021-06-04 11:13:24 +0800

    feat: add platform_config_tmp.mk file

---
+++ configs/platform/platform_config_tmp.mk
@@ -0,0 +1,22 @@
+# ===============================================================
+# 
+# Release under GPLv-3.0.
+# 
+# @file    platform_config_tmp.mk
+# @brief   
+# @author  gnsyxiang <gnsyxiang@163.com>
+# @date    04/06 2021 10:54
+# @version v0.0.1
+# 
+# @since    note
+# @note     note
+# 
+#     change log:
+#     NO.     Author              Date            Modified
+#     00      zhenquan.qiu        04/06 2021      create the file
+# 
+#     last modified: 04/06 2021 10:54
+# ===============================================================
+
+# 临时指定配置信息
+prefix_path := /opt/data/nfs/meian/smart_home/3rd_lib/$(project_target)
